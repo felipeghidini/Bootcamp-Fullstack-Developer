fa85dda7d17d61846b03570a77708da44c4b4f9d
felipeghidini <lipeghidini18@gmail.com>
2022-06-13 16:52:58 -0300

    Estudos de Diretivas Angular

--- explorando-diretivas/src/app/app.component.ts
+++ explorando-diretivas/src/app/app.component.ts
@@ -1,10 +1,50 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'explorando-diretivas';
+  count = 0;
+  nome = 'Felipe  Ghidini';
+  text: string = '';
+
+  pessoas = [
+    {
+      nome: 'Felipe',
+      sobrenome: 'Ghidini'
+    },
+    {
+      nome: 'Amanda',
+      sobrenome: 'Ghidini'
+    },
+    {
+      nome: 'Eloah',
+      sobrenome: 'Ghidini'
+    },
+    {
+      nome: 'Rafael',
+      sobrenome: 'Ghidini'
+    },
+  ]
+
+
+  constructor() {}
+
+  ngOnInit(): void {
+    let interval = setInterval(() => {
+      this.count++;
+      if (this.count === 10) {
+        clearInterval(interval);
+      }
+    }, 1000)
+  }
+
+  clicou(nome: string): void {
+    console.log('Clicou em mim', nome);
+  }
 }
+
+
