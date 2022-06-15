380134b8df12ca449932e942248e680ba38fd5b5
felipeghidini <lipeghidini18@gmail.com>
2022-06-15 15:14:51 -0300

    Implementando serviço com api externa

--- explorando-diretivas/src/app/app.module.ts
+++ explorando-diretivas/src/app/app.module.ts
@@ -1,25 +1,33 @@
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule} from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MarcaTextoDirective } from './shared/directives/marca-texto.directive';
 import { PeopleService } from './shared/services/people.service';
 import { ListPeopleComponent } from './shared/components/list-people/list-people.component';
+import { ListApiComponent } from './components/list-api/list-api.component';
+import { ListApiService } from './shared/services/list-api.service';
 
 @NgModule({
   declarations: [
     AppComponent,
     MarcaTextoDirective,
-    ListPeopleComponent
+    ListPeopleComponent,
+    ListApiComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule
   ],
-  providers: [PeopleService],
+  providers: [
+    PeopleService,
+    ListApiService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
