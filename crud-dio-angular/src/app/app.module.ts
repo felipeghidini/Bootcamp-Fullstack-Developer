6336caa4245d858fb94f2c7ed00fffeae832b6e2
felipeghidini <lipeghidini18@gmail.com>
2022-06-20 16:01:11 -0300

    Criando componente todo-lis e importando Angular Material

--- crud-dio-angular/src/app/app.module.ts
+++ crud-dio-angular/src/app/app.module.ts
@@ -1,6 +1,9 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http'
+import { MatInputModule } from '@angular/material/input';
+import { MatCardModule } from '@angular/material/card';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -11,6 +14,9 @@ import { UserService } from './services/user.service';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { DataBindingComponent } from './pages/data-binding/data-binding.component';
 import { ButtonComponent } from './components/button/button.component';
+import { TodoListComponent } from './pages/todo-list/todo-list.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatGridListModule } from '@angular/material/grid-list';
 
 @NgModule({
   declarations: [
@@ -19,14 +25,19 @@ import { ButtonComponent } from './components/button/button.component';
     UsersListComponent,
     UserFormComponent,
     DataBindingComponent,
-    ButtonComponent
+    ButtonComponent,
+    TodoListComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatCardModule,
+    MatGridListModule
   ],
   providers: [UserService],
   bootstrap: [AppComponent]
